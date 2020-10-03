96872c01daf9e8875e8e32b2e9e02601e920bddd
Mikaeyla <70242999+pechy28@users.noreply.github.com>
2020-10-02 21:43:34 -0400

    Animation commit

--- Assets/AudioScript.cs
+++ Assets/AudioScript.cs
@@ -10,11 +10,12 @@ public class AudioScript : MonoBehaviour
 
     public AudioClip musicClipTwo;
 
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,24 +25,38 @@ public class AudioScript : MonoBehaviour
         {
             musicSource.clip = musicClipOne;
             musicSource.Play();
+            anim.SetInteger("State", 1);
         }
 
         if (Input.GetKeyUp(KeyCode.W))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
         }
 
         if (Input.GetKeyDown(KeyCode.R))
         {
             musicSource.clip = musicClipTwo;
             musicSource.Play();
+            anim.SetInteger("State", 2);
         }
 
         if (Input.GetKeyUp(KeyCode.R))
         {
             musicSource.Stop();
+            anim.SetInteger("State", 0);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            musicSource.loop = true;
         }
 
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            musicSource.loop = false;
+        }
+        
         if (Input.GetKey("escape"))
         {
             Application.Quit();
